e0cbcddffb9097b7201a03beeab7074870db3242
JuoCode <gjdrift@gmail.com>
2016-03-11 16:07:30 +0800

    Fix cookie domain

--- src/scripts/signin.js
+++ src/scripts/signin.js
@@ -2,8 +2,9 @@ import $ from 'jquery';
 import Errors from './errors';
 
 const SIGNIN_URL = `${__API__}/login`;
-const COOKIE_KEY = `${__TARGET__}_session`;
+const COOKIE_KEY = __TARGET__ === 'production' ? 'flow_session' : `flow_session_${__TARGET__}`;
 const COOKIE_MAXAGE = 7*24*60*60;
+const COOKIE_DOMAIN = __TARGET__ === 'production' ? '.flow.ci' : '.lyon.flow.ci';
 
 const handleSignIn = (event) => {
   event.preventDefault();
@@ -16,7 +17,7 @@ const handleSignIn = (event) => {
   $.post(SIGNIN_URL, data)
     .done(resp => {
       const accessToken = resp.access_token;
-      document.cookie = `${COOKIE_KEY}=${accessToken}; domain=${__TARGET__}.flow.ci; max-age=${COOKIE_MAXAGE}`;
+      document.cookie = `${COOKIE_KEY}=${accessToken}; domain=${COOKIE_DOMAIN}.flow.ci; max-age=${COOKIE_MAXAGE}`;
       window.location = __TARGET__ === 'local' ? `${__DASHBOARD_URL__}?access_token=${accessToken}` : __DASHBOARD_URL__;
     })
     .fail(error => {

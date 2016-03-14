ce87c42a4c39f55a9b8264a786d45fd73054665f
cy <cy@fir.im>
2016-03-14 16:34:07 +0800

    注册页面显示sign 参数

--- src/scripts/signup.js
+++ src/scripts/signup.js
@@ -20,20 +20,23 @@ function validate (field) {
     errors.email = 'Invalid';
   }
 
-  if (sign) {
-    if (!username) {
-      errors.username = 'Required';
-    } else if (!USERNAME_REG.test(username)) {
-      errors.username = 'Invalid, length >= 3';
-    }
-    if (!password) {
-      errors.password = 'Required';
-    } else if (password.length < 6) {
-      errors.password = '6 characters minimum';
-    }
+  if(!sign) {
+    errors.sign = 'Required';
+  }
+
+  if (!username) {
+    errors.username = 'Required';
+  } else if (!USERNAME_REG.test(username)) {
+    errors.username = 'Invalid, length >= 3';
+  }
+  if (!password) {
+    errors.password = 'Required';
+  } else if (password.length < 6) {
+    errors.password = '6 characters minimum';
   }
   return errors;
 }
+
 function isEmptyObject (obj) {
   let isEmpty = true;
   for (let v in obj) {
@@ -41,17 +44,6 @@ function isEmptyObject (obj) {
     break;
   }
   return isEmpty;
-}
-function createSignLink (field) {
-  $.post({
-    url: `${__API__}/signup_link`,
-    data: field
-  }).done(function(resp) {
-    alert(`http://flow.ci/signup?sign=${resp.sign}&email=${resp.email}`);
-  }).fail(function(e) {
-    console.log('singup link error', e);
-    alert(Errors[e.responseJSON.code]);
-  })
 }
 
 function createUser (field) {
@@ -84,11 +76,8 @@ function handlerSubmit (event) {
     return false;
   }
 
-  if (field.sign) {
-    return createUser(field);
-  } else {
-    return createSignLink(field)
-  }
+  return createUser(field);
+
 }
 
 function getSearch () {
@@ -112,10 +101,6 @@ function injectQuery () {
   for(var prop in params) {
     let value = params[prop];
     $(`#${prop}`).val(value);
-  }
-  if (!params.sign) {
-    $(`#${USERNAME}`).hide();
-    $(`#${PASSWORD}`).hide();
   }
   $('#form-signup').submit(handlerSubmit);
 }

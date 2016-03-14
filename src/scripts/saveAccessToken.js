0af506fb4a388265cc6524a4cfc6b78cd55ecbe3
JuoCode <gjdrift@gmail.com>
2016-03-14 22:18:28 +0800

    temporary homepage

--- src/scripts/saveAccessToken.js
+++ src/scripts/saveAccessToken.js
@@ -2,7 +2,8 @@ const COOKIE_KEY = __TARGET__ === 'production' ? 'flow_session' : `flow_session_
 const COOKIE_MAXAGE = 7*24*60*60;
 const COOKIE_DOMAIN = __TARGET__ === 'production' ? '' : '.lyon';
 
-function saveAccessToken (accessToken) {
+const saveAccessToken = (accessToken) {
   document.cookie = `${COOKIE_KEY}=${accessToken}; domain=${COOKIE_DOMAIN}.flow.ci; path='/'; max-age=${COOKIE_MAXAGE}`;
-}
+};
+
 export default saveAccessToken;

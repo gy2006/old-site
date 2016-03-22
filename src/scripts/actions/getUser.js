2cf3da44bf987cf9eb48e8c42e7c632f72f5fd03
cy <cy@fir.im>
2016-03-22 17:13:21 +0800

    增加mixpanel people 设置 及事件关联

--- src/scripts/actions/getUser.js
+++ src/scripts/actions/getUser.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import { GETUSER_URL } from '../constant';
+import analysis from './analysis';
 
 let USER;
 let PREV_TOKEN;
@@ -8,8 +9,9 @@ export default function (token) {
   //   return USER;
   // }
   return $.ajax(`${GETUSER_URL}?access_token=${token}`, {
-    method: 'head'
+    method: 'get'
   }).done((resp)=>{
     USER = resp;
+    analysis.identify(resp.email);
   })
 }

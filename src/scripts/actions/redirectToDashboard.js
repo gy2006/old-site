3fc372c0221bd39ae1fbe8ee9ae826bf998d87b0
Anm <7972296@qq.com>
2016-03-23 17:03:02 +0800

    modified:   src/scripts/actions/redirectToDashboard.js 	modified:   src/scripts/constant.js 	modified:   src/scripts/util/cookies.js 添加重定向到验证token失败前的地址

--- src/scripts/actions/redirectToDashboard.js
+++ src/scripts/actions/redirectToDashboard.js
@@ -1,4 +1,24 @@
+import {
+  get as getCookie,
+  clear as clearCookie
+} from '../util/cookies';
+import { REDIRECT_KEY } from '../constant';
+
 export default function redirect (accessToken) {
-  window.location = __TARGET__ === 'local' ? `${__DASHBOARD_URL__}?access_token=${accessToken}` : __DASHBOARD_URL__;
+  let redirectUrl = getCookie(REDIRECT_KEY);
+
+  if (redirectUrl) {
+    redirectUrl = decodeURIComponent(redirectUrl);
+    if (redirectUrl.indexOf(__DASHBOARD_URL__) === 0) {
+      redirectUrl = redirectUrl;
+    } else {
+      redirectUrl = __TARGET__ === 'local' ? `${__DASHBOARD_URL__}?access_token=${accessToken}` : __DASHBOARD_URL__;
+    }
+    clearCookie(REDIRECT_KEY);
+  } else {
+    redirectUrl = __TARGET__ === 'local' ? `${__DASHBOARD_URL__}?access_token=${accessToken}` : __DASHBOARD_URL__;
+  }
+
+  window.location = redirectUrl;
 }
 
--- src/scripts/constant.js
+++ src/scripts/constant.js
@@ -2,6 +2,7 @@ export const EMAIL_REG = /^[a-zA-Z0-9_+.-]+\@([a-zA-Z0-9-]+\.)+[a-zA-Z0-9]{2,6}/
 export const USERNAME_REG = /^[0-9a-zA-Z]{3,15}$/;
 
 export const COOKIE_KEY = __TARGET__ === 'production' ? 'flow_session' : `flow_session_${__TARGET__}`;
+export const REDIRECT_KEY = __TARGET__ === 'production' ? 'flow_session_redirect' : `flow_session_${__TARGET__}_redirect`;
 
 export const SIGNIN_URL = `${__API__}/login`;
 export const SIGNUP_URL = `${__API__}/signup`;

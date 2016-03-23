5dfecb90fa794c312ca72c3da739c92f6b053823
cy <cy@fir.im>
2016-03-23 11:45:27 +0800

    remove User cache

--- src/scripts/actions/user.js
+++ src/scripts/actions/user.js
@@ -2,18 +2,10 @@ import $ from 'jquery';
 import { GETUSER_URL } from '../constant';
 import analysis from './analysis';
 
-let USER;
-let PREV_TOKEN;
 export function getUser (token) {
-  if (USER && PREV_TOKEN === token) {
-    return USER;
-  }
-  PREV_TOKEN = token;
   return $.ajax(`${GETUSER_URL}?access_token=${token}`, {
     method: 'get'
-  }).done((resp)=>{
-    USER = resp;
-  })
+  });
 }
 export function testUser(token) {
   return $.ajax(`${GETUSER_URL}?access_token=${token}`, {

d07a19ac6aaf08390093452a821b3bd54f90ff85
cy <cy@fir.im>
2016-03-24 18:05:40 +0800

    fix redirectToDashboard

--- src/scripts/actions/redirectToDashboard.js
+++ src/scripts/actions/redirectToDashboard.js
@@ -4,21 +4,18 @@ import {
 } from '../util/cookies';
 import { REDIRECT_KEY } from '../constant';
 
-export default function redirect (accessToken) {
-  let redirectUrl = getCookie(REDIRECT_KEY);
+function redirectToDefault (accessToken) {
+  return __TARGET__ === 'local' ? `${__DASHBOARD_URL__}?access_token=${accessToken}` : __DASHBOARD_URL__;
+}
 
-  if (redirectUrl) {
-    redirectUrl = decodeURIComponent(redirectUrl);
-    if (redirectUrl.indexOf(__DASHBOARD_URL__) === 0) {
-      redirectUrl = redirectUrl;
-    } else {
-      redirectUrl = __TARGET__ === 'local' ? `${__DASHBOARD_URL__}?access_token=${accessToken}` : __DASHBOARD_URL__;
-    }
-    clearCookie(REDIRECT_KEY);
-  } else {
-    redirectUrl = __TARGET__ === 'local' ? `${__DASHBOARD_URL__}?access_token=${accessToken}` : __DASHBOARD_URL__;
+export default function redirectToDashboard (accessToken) {
+  let url = getCookie(REDIRECT_KEY);
+  url && clearCookie(REDIRECT_KEY);
+
+  if (!url || url.indexOf(__DASHBOARD_URL__) !== 0) {
+    url = redirectToDefault(accessToken);
   }
 
-  window.location = redirectUrl;
+  window.location = url;
 }
 

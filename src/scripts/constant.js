a67cb2e8120833399d8b6fafc24ceec401fd21e9
cy <cy@fir.im>
2016-03-25 11:12:21 +0800

    fix redirect_key name

--- src/scripts/constant.js
+++ src/scripts/constant.js
@@ -2,7 +2,7 @@ export const EMAIL_REG = /^[a-zA-Z0-9_+.-]+\@([a-zA-Z0-9-]+\.)+[a-zA-Z0-9]{2,6}/
 export const USERNAME_REG = /^[0-9a-zA-Z]{3,15}$/;
 
 export const COOKIE_KEY = __TARGET__ === 'production' ? 'flow_session' : `flow_session_${__TARGET__}`;
-export const REDIRECT_KEY = __TARGET__ === 'production' ? 'flow_session_redirect' : `flow_session_${__TARGET__}_redirect`;
+export const REDIRECT_KEY = __TARGET__ === 'production' ? 'flow_previous_url' : `flow_${__TARGET__}_previous_url`;
 
 export const SIGNIN_URL = `${__API__}/login`;
 export const SIGNUP_URL = `${__API__}/signup`;

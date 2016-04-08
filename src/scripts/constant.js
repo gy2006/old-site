b23f669f21893ee32d4f58e155a3844fbdae5795
cy <cy@fir.im>
2016-04-08 14:46:38 +0800

    修改username命名规则

--- src/scripts/constant.js
+++ src/scripts/constant.js
@@ -1,5 +1,6 @@
 export const EMAIL_REG = /^[a-zA-Z0-9_+.-]+\@([a-zA-Z0-9-]+\.)+[a-zA-Z0-9]{2,6}/i;
-export const USERNAME_REG = /^[0-9a-zA-Z]{3,15}$/;
+// 已字母开头，数字或字母结尾
+export const USERNAME_REG = /^[a-zA-Z]\w{1,13}[a-zA-Z0-9]$/;// /^[0-9a-zA-Z]{3,15}$/;
 
 export const COOKIE_KEY = __TARGET__ === 'production' ? 'flow_session' : `flow_session_${__TARGET__}`;
 export const REDIRECT_KEY = __TARGET__ === 'production' ? 'flow_previous_url' : `flow_${__TARGET__}_previous_url`;

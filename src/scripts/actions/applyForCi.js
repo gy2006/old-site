143204320abbee2da8e3d4140d34ce125f41c52c
cy <cy@fir.im>
2016-04-14 17:08:43 +0800

    修改使用 mixpanel.alias 的判断逻辑
    只有当 distinct_id 为 mixpanel 自动生成的 id 才使用 alias

--- src/scripts/actions/applyForCi.js
+++ src/scripts/actions/applyForCi.js
@@ -1,8 +1,7 @@
 import analysis from './analysis';
-import User from './user';
 
 function noop () {}
 
 export default function (fields, callback = noop) {
-  analysis.event.applyCi(fields, User.getUserToken(), callback);
+  analysis.event.applyCi(fields, callback);
 }

be561b26d8fc22c8a0dd98e71556216523362df8
cy <cy@fir.im>
2016-09-21 17:32:53 +0800

    修改 cookie domain

--- src/scripts/constant.js
+++ src/scripts/constant.js
@@ -2,8 +2,17 @@ export const EMAIL_REG = /^[a-zA-Z0-9_+.-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z0-9]{2,6}/i
 // 已字母开头，数字或字母结尾
 export const USERNAME_REG = /^\w{3,15}$/// /^[0-9a-zA-Z]{3,15}$/;
 
+// set default cookie config
+let host = location.hostname.split('.')
+let domain = host
+// 子域名取上层域名
+if (!/^\d{1,3}\.\d{1,3}\.\d{1,3}/.test(host) && host.length > 2) {
+  host[0] = ''
+  domain = host.join('.')
+}
+
 export const SESSION_COOKIE_MAXAGE = 7 * 24 * 60 * 60
-export const COOKIE_DOMAIN = { local: 'localhost', lyon: '.lyon.flow.ci', production: '.flow.ci' }[__TARGET__]
+export const COOKIE_DOMAIN = domain
 
 export const BASE_COOKIE_CONFIG = {
   path: '/',
@@ -14,7 +23,8 @@ export const SESSION_COOKIE_CONFIG = Object.assign({}, BASE_COOKIE_CONFIG, {
   'max-age': SESSION_COOKIE_MAXAGE
 })
 
-export const COOKIE_KEY = __TARGET__ === 'production' ? 'flow_session' : `flow_session_${__TARGET__}`
+export const COOKIE_KEY = 'flow_session'
+// export const COOKIE_KEY = __TARGET__ === 'production' ? 'flow_session' : `flow_session_${__TARGET__}`
 
 export const REDIRECT_KEY = __TARGET__ === 'production' ? 'flow_previous_url' : `flow_${__TARGET__}_previous_url`
 

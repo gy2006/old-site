e033c1cb7d86f1c97bb4cd4e65ba2ff95185bc41
cy <cy@fir.im>
2016-09-28 17:53:36 +0800

    强制刷新页面

--- src/scripts/actions/setLocale.js
+++ src/scripts/actions/setLocale.js
@@ -5,5 +5,5 @@ export default function (locale) {
   cookies.save('locale', locale, Object.assign({}, BASE_COOKIE_CONFIG, {
     'max-age': 365 * 24 * 60 * 60
   }))
-  window.location.reload()
+  window.location.reload(true)
 }

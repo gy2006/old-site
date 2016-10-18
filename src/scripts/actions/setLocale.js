7a91560d4ea2dd1f32b1d96a835317da4f1c14c4
cy <cy@fir.im>
2016-10-18 16:33:30 +0800

    fix setlocale

--- src/scripts/actions/setLocale.js
+++ src/scripts/actions/setLocale.js
@@ -5,10 +5,11 @@ export default function (locale) {
   cookies.save('locale', locale, Object.assign({}, BASE_COOKIE_CONFIG, {
     'max-age': 365 * 24 * 60 * 60
   }))
-  // const href = window.location.href
-  // if (href.indexOf('?') > -1) {
-  //   window.location.href = `${href}&d=${(new Date()).getTime()}`
-  // } else {
-  //   window.location.href = `${href}?d=${(new Date()).getTime()}`
-  // }
+  // 防止浏览器缓存
+  const href = window.location.href
+  if (href.indexOf('?') > -1) {
+    window.location.href = `${href}&d=${(new Date()).getTime()}`
+  } else {
+    window.location.href = `${href}?d=${(new Date()).getTime()}`
+  }
 }

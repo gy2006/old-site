158662ccf2ff4ad8dffc0be7f577f530972b33a3
cy <cy@fir.im>
2016-11-02 15:52:31 +0800

    添加 注册统计人物属性

--- src/scripts/actions/analysis.js
+++ src/scripts/actions/analysis.js
@@ -140,28 +140,6 @@ const analysis = {
     const props = { path: location.pathname, protocol: location.protocol }
     trackFullEvent(eventName, props, [], { noPrevious: true })
   },
-  alias: function (newId) {
-    // must only use create mixpanel people
-    const oldId = mixpanel.get_distinct_id()
-    /**
-      1. 对于同一个 distinct_id, alias 只能在第一次生效
-      2. 当前逻辑是只有当 distinct_id 为 mixpanel 自动生成的 id 才使用 alias。
-      3. 如果在同一个浏览器多次申请，在第二次及之后申请的时候在 alias 会使 distinct_id 设置失效，所以判断是否已经alias。 在第三次时，此时 alias 已经为空，但是 distinct_id 为邮箱，所以在加判断 distinct_id 是否为邮箱
-    **/
-    // 如果是uuid 则为mixpanel自动生成，则alias， 否则用 identify
-    try {
-      const hasAlias = mixpanel.persistence.props.__alias
-      if (hasAlias) {
-        mixpanel.identify(newId)
-        return
-      }
-    } catch (e) {}
-    if (EMAIL_REG.test(oldId)) {
-      mixpanel.identify(newId)
-    } else {
-      mixpanel.alias(newId, oldId)
-    }
-  },
   trackAlias: function (newId) {
     const oldId = mixpanel.get_distinct_id()
     // old distinct_id event
@@ -180,12 +158,6 @@ const analysis = {
     mixpanelVariables.setValue(property)
   },
   common: {
-    runWithDistinctId: function (id, callFn) {
-      const nowDistinctId = mixpanel.get_distinct_id()
-      analysis.identify(id)
-      callFn && callFn()
-      analysis.identify(nowDistinctId)
-    },
     getCreatePeopleProperty: function (fields) {
       const utm = {}
       UTM_LIST.forEach((key) => {
@@ -197,7 +169,11 @@ const analysis = {
       const people = {
         '$email': fields.email,
         'Apply_At': new Date(),
-        'Application': 'apply'
+        'Application': 'apply',
+        'Company': fields.company_name,
+        '职位': fields.job,
+        'telephone': fields.telephone,
+        '开发团队规模': fields.company_scale
       }
       return Object.assign({}, people, utm)
     }
@@ -212,7 +188,9 @@ const analysis = {
       mixpanel.people.increment('signed_in', 1, callback)
     },
     signUp: function (user, urlParams, callback) {
-      analysis.alias(user.email)
+      mixpanel.alias(user.email)
+      const people = analysis.common.getCreatePeopleProperty(fields)
+      mixpanel.people.set_once(people)
       const isInvited = !!urlParams.project_id
       trackFullEvent('Sign up', {
         Invited: isInvited ? 'YES' : 'NO'
